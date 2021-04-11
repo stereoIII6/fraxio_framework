1035ed95580fd186dda9450dd732e8d3d855d55a
stereoIII6 <stereodocbush@gmail.com>
2021-04-11 23:15:13 +0200

    moonboyz

--- src/components/Media.js
+++ src/components/Media.js
@@ -5,12 +5,15 @@ import { InputGroup, Input, Form } from 'reactstrap';
 class Media extends Component {
     static propTypes = {
         layers: PropTypes.array,
+        activeLayer: PropTypes.number,
         activeKey: PropTypes.number
     };
 
     render() {
         const btn_colors = ["MediumSeaGreen", "LimeGreen", "cornflowerblue", "violet", "tomato", "orange"];
         const colori = btn_colors[this.props.activeKey];
+        let activeObj = null;
+            
         return (
             <div id="layers" className="col-9 p-0 m-0" style={{ height: "450px", background: colori, color: "ivory" }}>
                 <div className="row">
@@ -21,8 +24,22 @@ class Media extends Component {
                                 this.props.layers.map(laya => (
 
                                     <div key={laya.key} style={{ width: "750px", height: "430px", position: "absolute", top: "0", left: "0", overflow: "hidden" }}>
-                                        {console.log(laya.key, this.props.layers[laya.key], "media log"), this.props.layers[laya.key].obj.start ?
-                                            <img id="layers[layer].obj.alpha" src={`https://ipfs.io/ipfs/${laya.path}`} style={{ position: "absolute", top: `${laya.obj.start.y}px`, left: `${laya.obj.start.x}px`, width: `${(laya.obj.start.z / 100 * ((750) - 20))}px`, opacity: `${laya.obj.start.o / 100}`, transform: `rotate(${laya.obj.start.r}deg)` }} />
+                                        {
+
+                                             
+                                         (this.props.activeKey === 0) ? activeObj = laya.obj.top : null,
+                                                (this.props.activeKey === 1) ? activeObj = laya.obj.mid : null,
+                                                    (this.props.activeKey === 2) ? activeObj = laya.obj.start : null,
+                                                        (this.props.activeKey === 3) ? activeObj = laya.obj.low : null,
+                                                            (this.props.activeKey === 4) ? activeObj = laya.obj.bottom : null,
+                                                                (this.props.activeKey === 5) ? activeObj = laya.obj.custom : null
+
+                                            }
+                                                        {
+
+                                           // console.log(laya.key, this.props.layers[laya.key], "media log"), this.props.layers[laya.key].obj.start 
+                                           activeObj ?
+                                            <img id="layers[layer].obj.alpha" src={`https://ipfs.io/ipfs/${laya.path}`} style={{ position: "absolute", top: `${activeObj.y}px`, left: `${activeObj.x}px`, width: `${(activeObj.z / 100 * ((750) - 20))}px`, opacity: `${activeObj.o / 100}`, transform: `rotate(${activeObj.r}deg)` }} />
                                             :
                                             <img id="layers[layer].obj.alpha" src={`https://ipfs.io/ipfs/${laya.path}`} style={{ position: "absolute", top: `${laya.obj.alpha.y}px`, left: `${laya.obj.alpha.x}px`, width: `${(laya.obj.alpha.z / 100 * ((750) - 20))}px`, opacity: `${laya.obj.alpha.o / 100}`, transform: `rotate(${laya.obj.alpha.r}deg)` }} />
                                         }
