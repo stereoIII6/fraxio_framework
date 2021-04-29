203965cc2351166221e1a6021cd04488c1359405
stereoIII6 <stereodocbush@gmail.com>
2021-04-29 22:31:43 +0200

    moon to go

--- src/components/subs/Navigation/TopMenu.js
+++ src/components/subs/Navigation/TopMenu.js
@@ -36,7 +36,10 @@ class TopMenu extends Component {
                         </div>
                     </div>
                     <div className="col" style={{ textAlign: "right" }}>
-                        <div className="btn p-2" onClick={this.onClick}
+                        <div className="btn p-2 mr-1" onClick={this.onClick} style={{ background: sky, color: blue, borderRadius: "9px" }} id="mlk_lnk">
+                            0 MLK
+                        </div>
+                        <div className="btn p-2 mr-1" onClick={this.onClick}
                             id="dashboard_lnk"
                             style={{ color: fresh, background: purple, fontWeight: 900, borderRadius: "9px" }} >
                             <Blockies
@@ -48,6 +51,11 @@ class TopMenu extends Component {
                                 scale={3}
                                 spotcolor="#000"
                             />{" account[0] "}
+                        </div>
+                        <div className="btn p-2" onClick={this.onClick} style={{background:blue, color: fresh,borderRadius: "9px"}} id="accmenu_lnk">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                            </svg>
                         </div>
                     </div></div>
                 <div className="row">
