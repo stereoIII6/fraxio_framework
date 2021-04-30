a050916493450432bc2a69e352e28bb2f6d1e945
stereoIII6 <stereodocbush@gmail.com>
2021-04-30 03:13:00 +0200

    moon to go

--- src/components/subs/RoadMap/RoadMap.js
+++ src/components/subs/RoadMap/RoadMap.js
@@ -5,9 +5,10 @@ class RoadMap extends Component {
     render() {
         return (<div style={{ position: "relative", top: 0, left: 0 }} ><h1>Road Map</h1><hr></hr>
             <div style={{ position: "relative", top: 0, left: 0 }}>
-                <div style={{ position: "absolute", top: 0, left: "-2em", paddingBottom: "40px" }} > <img src="../../../Road.png" style={{ background: grey, borderRadius: "9px", paddingTop: "20px", }} /></div>
-                <div style={{ position: "absolute", top: 20, left: 0 }} ><img src="../../../2q21_down.png" /></div>
-                <div style={{ position: "absolute", top: 20, left: 0 }} ><img src="../../../2q21_up.png" /></div>
+                <div style={{ position: "absolute", top: 20, left: 0 }} > <img src="../../../bg.png" style={{ width: "100%" }} /></div>
+                <div style={{ position: "absolute", top: 20, left: 0 }} > <img src="../../../Road.png" style={{ width: "100%" }} /></div>
+                <div style={{ position: "absolute", top: 20, left: 0 }} ><img src="../../../2q21_down.png" style={{width: "100%"}} /></div>
+                <div style={{ position: "absolute", top: 20, left: 0 }} ><img src="../../../2q21_up.png" style={{width: "100%"}} /></div>
 
             </div>
         </div>);
