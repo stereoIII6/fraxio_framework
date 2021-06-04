c4c8a436107ce9f0de28281d1669ca7472279b25
stereoIII6 <stereodocbush@gmail.com>
2021-06-05 00:04:39 +0200

    venus

--- src/components/subs/Landing/Landing.js
+++ src/components/subs/Landing/Landing.js
@@ -3,8 +3,20 @@ class Landing extends Component {
     state = {  }
     render() { 
         return ( <div>
-            <SlideShow></SlideShow>
-            <Videos></Videos>
+            <div className="jumbotron">
+
+            </div>
+            <div className="row">
+                <div className="col-4 mr-5" style={{ opacity:"0.3"}}>
+                    <h1>Welcome to the Fractio Framework</h1>
+                    <h3>
+                        
+                    </h3>
+                </div>
+                <div className="col-7 alert alert-dark">
+                    <img src="" alt="" style={{width: "560px", height: "420px"}} />
+                </div>
+            </div>
         </div> );
     }
 }
