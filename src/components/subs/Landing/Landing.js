294e6d4d4548420a4f8098046f41511dbcccf490
stereoIII6 <stereodocbush@gmail.com>
2021-06-05 03:04:37 +0200

    venus

--- src/components/subs/Landing/Landing.js
+++ src/components/subs/Landing/Landing.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
+import "./styles.css";
+import YoutubeEmbed from "./YoutubeEmbed";
 class Landing extends Component {
     state = {  }
     render() { 
         return ( <div>
-            <div className="jumbotron">
-
+            <div className="slideshow" style={{ position: "relative", top: "0", background: "tomato", height: "220px", marginBottom:"2em", overflow: "hidden" }}>
+                <div className="slider" style={{ position: "relative", top: "0" , left: "0", width: "400%", height: "220px", background: "lime"}}>
+                    <div className="slide" id="slide1" style={{ textAlign: "center", paddingTop: "3em", float: "left", width:"25%", height: "220px", overflow: "hidden", background: "url('./one.png')" }}>
+                        <h2 style={{ background: "none", color: "#9fe6c3", width: "auto"}} >GROWING INTO THE FUTURE ...</h2>
+                    </div>
+                    <div className="slide" id="slide2" style={{ float: "left", width: "25%", height: "220px", overflow: "hidden", background: "lightblue" }}>
+                        <img src="./two.png" alt="" style={{ height: "220px" }} />
+                    </div>
+                    <div className="slide" id="slide3" style={{ float: "left", width: "25%", height: "220px", overflow: "hidden", background: "orange" }}>
+                        <img src="./three.png" alt="" style={{ height: "220px" }} />
+                    </div>
+                    <div className="slide" id="slide3" style={{ float: "left", width: "25%", height: "220px", overflow: "hidden", background: "lightblue" }}>
+                        <img src="./four.png" alt="" style={{ height: "220px" }} />
+                    </div>
+                </div>
             </div>
+            <h1 style={{ fontSize: "5em", fontWeight: "900", marginBottom: "1em", background: "#9f95c3", color: "#e2e3db", textAlign: "center", borderRadius : "9px"}}>GIVE ME SOME MILQ</h1>
             <div className="row">
-                <div className="col-4 mr-5" style={{ opacity:"0.3"}}>
-                    <h1>Welcome to the Fractio Framework</h1>
+                <div className="col-4" style={{ opacity:"1"}}>
+                    <h1>Welcome to the Fractio Framework </h1>
                     <h3>
-                        
+
                     </h3>
                 </div>
-                <div className="col-7 alert alert-dark">
-                    <img src="" alt="" style={{width: "560px", height: "420px"}} />
+                <div className="col-8">
+                    <YoutubeEmbed embedId="1717zs_e9zw" />
                 </div>
             </div>
         </div> );
